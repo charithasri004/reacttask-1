ffa8d2e819d4d7f1e229c84d51d410ba1e0e2ae1
charitha sri <charithasri004@gmail.com>
2023-10-20 21:48:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import {Component} from "react"
 import './App.css';
 
-function App() {
+class App extends Component {
+  state={count:0}
+
+  onChangeCount = (event)=>{
+    if(event.target.value.trim().length===0){
+      this.setState({count:0})
+    }
+    else{
+      this.setState({count:event.target.value.trim().split(" ").length})
+    }
+   
+  }
+ render(){
+  const {count} = this.state
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className='word-count-container'>
+          <h1 className='word-count-heading'>Responsive Paragraph Word Counter</h1>
+          <textarea cols={40} rows={6} className='input-words' onChange={this.onChangeCount}/>
+          <p style={{alignSelf:"flex-start",marginLeft: "50px"}}>Word Count:{count}</p>
+        </div>
     </div>
   );
+ }
+  
 }
 
 export default App;
